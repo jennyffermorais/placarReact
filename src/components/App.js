d9856a670bcee5e5be850a65c7ea33584a5e5985
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-07-26 19:29:42 -0300

    primeiro commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import PlacarContainer from "./PlacarContainer";
+
+const dados = {
+  partida: {
+    estadio: "Mineirão/MG",
+    data: "08/07/2014",
+    horario: "17h",
+  },
+  casa: {
+    nome: "Brasil",
+  },
+  visitante: {
+    nome: "Alemanha",
+  },
+};
+
+export default class App extends React.Component {
+  render() {
+    return <PlacarContainer {...dados} />;
+  }
+}
